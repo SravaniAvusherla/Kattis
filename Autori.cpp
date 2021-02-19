bbe682e0865010a6570fad325745d4778cd80e47
SravaniAvusherla <63154142+SravaniAvusherla@users.noreply.github.com>
2021-02-19 09:45:18 +0100

    Create Autori.cpp

---
+++ Autori.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string l;
+    cin >> l;
+    
+    string input = string(1, l[0]);
+    
+    for (int i =1; i < l.size() -1; i++) 
+    {
+        if (l[i] == '-') {
+            input += string(1, l[i+1]);
+        }
+    }
+    
+     cout << input ;
+   }
